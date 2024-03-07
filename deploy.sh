9195549947f7802c877fc97ca405199f93cbab7b
김도형 <dohyung97022@gmail.com>
2024-03-07 19:21:50 +0900

    [#11] deploy.sh 명령어 개선.

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,4 @@ docker build -t dohyung97022/aws-lambda-clone:latest .
 docker push dohyung97022/aws-lambda-clone:latest
 ssh master mkdir "~/aws-lambda-clone"
 scp -r "./kubernetes" "master:~/aws-lambda-clone"
-
-## deploy kubernetes
-#ssh master
-#cd ~/aws-lambda-clone/kubernetes
-#kubectl apply -f .
+ssh master "cd ~/aws-lambda-clone/kubernetes; kubectl apply -f ."
